18fa5dc817297db84b647bea34da9a8bd0bb7fec
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-12-09 00:55:18 +0900

    javascript作成

---
+++ app/javascript/category.js
@@ -0,0 +1,11 @@
+window.addEventListener("turbo:load", function() {
+  
+  const parentCategory = document.getElementById("parent-category")
+
+  const selectChildElement = (selectForm) => {
+
+  }
+  pernetCategory.addEventlistener("change", function () {
+    selectChildElement("child-select-wrap")
+  })
+ })
